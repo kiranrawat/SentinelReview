9026063bf5d9f4c7ed1ccf4b4e4ec1604d367f21
Nikhil Tyagi <nikhil.tyagi62@gmail.com>
2026-01-07 18:25:10 -0800

    Feat/create app and mcp (#6)
    * feat: MCP and api
    
    * feat: create frontend, fixed api

---
+++ src/api/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from agents.PRReviewer import PRReviewer
+from api.models import PRUrl
+
+app = FastAPI(
+    title="Sentinel Review API", 
+    version="1.0.0"
+)
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/review-pr", tags=["Review PR"])
+async def review_pr(pr_url: PRUrl):
+    pr_reviewer = PRReviewer()
+    pr_review_response = await pr_reviewer.generate_pr_review(pr_url=pr_url)
+    return {'pr_url': pr_url, 'review': pr_review_response}
